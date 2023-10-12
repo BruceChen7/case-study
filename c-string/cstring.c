79324f9aa73892837cbb1aee07473964465a4fcb
BruceChen7 <bofengqiye7@gmail.com>
2023-10-12 23:12:58 +0800

    feat: add cstring

---
+++ c-string/cstring.c
@@ -0,0 +1,25 @@
+#include "cstring.h"
+
+typedef struct s8 {
+    u8* buf;
+    size len;
+} s8;
+
+
+s8 s8Span(u8* begin, u8* end) {
+    s8 s;
+    s.buf = begin;
+    s.len = end - begin;
+    return s;
+}
+
+size s8Cmp(s8 a, s8 b) {
+    size len = a.len < b.len ? a.len: a.len;
+    for (size i = 0; i < len; i++) {
+        size d = a.buf[i] - b.buf[i];
+        if (d != 0) {
+            return d;
+        }
+    }
+    return a.len - b.len;
+}
---
+++ c-string/cstring.h
@@ -0,0 +1,30 @@
+#ifndef __C_STRING_H__
+#define __C_STRING_H__
+// https://github.com/skeeto/scratch/blob/master/misc/treap.c
+#include <stddef.h>
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef int32_t b32;
+typedef int32_t i32;
+typedef uint64_t u64;
+typedef char byte;
+typedef size_t usize;
+typedef ptrdiff_t size;
+
+#define sizeof(x) (size)sizeof(x)
+#define alignof(x) (size)_Alignof(x)
+#define countof(x) (size)sizeof(x)/sizeof(*x)
+#define lengthof(s) (countof(s) - 1)
+
+#define S(s) (s8) { (u8 *)(s), lengthof(s)}
+
+typedef struct s8  s8;
+
+s8 s8Span(u8 *begin, u8* end);
+
+size s8Cmp(s8 a, s8 b);
+
+
+
+#endif //
