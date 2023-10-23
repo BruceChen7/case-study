b52358606f6280c52fbbd3f9f1a724db374e1329
BruceChen7 <bofengqiye7@gmail.com>
2023-10-23 18:44:36 +0800

    test: add rsp tests

--- zigredis/build.zig
+++ zigredis/build.zig
@@ -58,17 +58,25 @@ pub fn build(b: *std.Build) void {
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const unit_tests = b.addTest(.{
+    const req_unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/req.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const rsp_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/rsp.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(req_unit_tests);
+    const run_rsp_unit_tests = b.addRunArtifact(rsp_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+    test_step.dependOn(&run_rsp_unit_tests.step);
 }
