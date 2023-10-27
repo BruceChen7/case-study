6b4894559ff3d979fb194d2758906354cf260876
BruceChen7 <bofengqiye7@gmail.com>
2023-10-27 18:59:42 +0800

    feat: more impl

--- zig-caskdb/src/db.zig
+++ zig-caskdb/src/db.zig
@@ -3,12 +3,12 @@ const option = @import("option.zig");
 const disk = @import("disk.zig");
 const directory = @import("dir.zig");
 
-const ArchiveFileList = std.ArrayList([]disk.File);
+const ArchiveFileList = std.ArrayList([]disk.CaskFile);
 pub const DB = struct {
     allocator: std.mem.Allocator,
-    activeFile: ?disk.File,
+    activeFile: ?*disk.CaskFile,
     archiveFile: ArchiveFileList,
-    mergeFile: ?disk.File,
+    mergeFile: ?disk.CaskFile,
     options: *option.Option,
     pendingWring: []const u8,
 
@@ -34,13 +34,35 @@ pub const DB = struct {
     ) void {
         self.archiveFile.deinit();
         self.allocator.destroy(self.options);
+        if (self.activeFile) |file| {
+            file.deinit();
+        }
     }
 
     pub fn open(
         self: *DB,
     ) !void {
         const dir = directory.Dir.init(self.options.mergeFileDir);
-        try dir.getSpecificExtFile(self.options.mergefileExt, self.allocator);
+        var mergeFileList = try dir.getSpecificExtFile(self.options.mergefileExt, self.allocator);
+        _ = mergeFileList;
+        var segmentFileList = try dir.getSpecificExtFile(self.options.segmentfileExt, self.allocator);
+        // 没有文件
+        if (segmentFileList.items.len == 0) {
+            // 新创建一个segment 文件，并
+            var file = try disk.CaskFile.create(self.allocator, 0, disk.FileType.SEGMENT, self.options.segmentFileExt, self.options.segmentFileDir);
+            self.activeFile = file;
+            try self.activeFile.?.open();
+            try self.activeFile.?.seekLast();
+        } else {
+            // 按照文件名来排序
+            std.sort.sort([][]const u8, segmentFileList.items, {}, struct {
+                fn lessThan(_: void, a: []const u8, b: []const u8) bool {
+                    return std.mem.lessThan(u8, a, b);
+                }
+            }.lessThan);
+            var lastFile: []u8 = segmentFileList.items[segmentFileList.items.len - 1];
+            _ = lastFile;
+        }
     }
 
     pub fn store(self: *DB, key: []const u8, value: []const u8) !void {
--- zig-caskdb/src/disk.zig
+++ zig-caskdb/src/disk.zig
@@ -1,8 +1,10 @@
-const DiskEntry = struct {
+const std = @import("std");
+const FileEntry = struct {
     keySize: u32,
     valueSize: u32,
     key: []const u8,
     value: []const u8,
+    path: []const u8,
 };
 
 const KeyDirEntry = struct {
@@ -22,8 +24,42 @@ const FileType = enum {
     MERGE,
 };
 
-pub const File = union(FileType) {
-    SEGMENT: DiskEntry,
-    WAL: DiskEntry,
-    MERGE: DiskEntry,
+pub const CaskFile = struct {
+    fileID: u32,
+    fileType: FileType,
+    path: []const u8,
+    alloc: std.mem.Allocator,
+    file: ?std.fs.File,
+
+    pub fn create(alloc: std.mem.Allocator, fileID: u32, fileType: FileType, ext: []const u8, dir: std.fs.Dir) !*CaskFile {
+        var dirPath = try dir.realpathAlloc(std.testing.allocator, ".");
+        var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+        var subPath = try std.fmt.bufPrint(buf, "{d}{s}", .{ fileID, ext });
+        var name = try std.fs.path.join(alloc, &.{ dirPath, subPath });
+
+        try dir.createFile(subPath, .{ .truncate = true, .read = true, .exclusive = true });
+        return .{
+            .alloc = alloc,
+            .fileID = fileID,
+            .fileType = fileType,
+            .path = name,
+            .file = null,
+        };
+    }
+
+    pub fn open(self: *CaskFile) !void {
+        self.file = try std.fs.openFileAbsolute(self.path, .{ .read_write = true, .exclusive = true });
+    }
+    pub fn seekLast(self: *CaskFile) !void {
+        if (self.file) |f| {
+            try f.seekFromEnd(f.getEndPos());
+        }
+    }
+
+    pub fn deinit(self: *CaskFile) void {
+        self.alloc.free(self.path);
+        if (self.file) |f| {
+            f.close();
+        }
+    }
 };
