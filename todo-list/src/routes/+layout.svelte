669f8704e888302ad75baab5c3105457ba0376ef
BruceChen7 <bofengqiye7@gmail.com>
2023-11-28 22:27:23 +0800

    feat: add more impl

--- todo-list/src/routes/+layout.svelte
+++ todo-list/src/routes/+layout.svelte
@@ -2,50 +2,55 @@
     import "../app.css";
 </script>
 
-<div class="mt-52">
-    <div class="grid grid-cols-4 gap-7">
-        <div class="grid grid-flow-row auto-rows-max">
-            <div class="text-center border border-x-gray-200 rounded-md">
-                <p class="bg-green-500">待办列表</p>
-            </div>
-            <div class="border-gray-300">
-                <div class="text-center border border-gray-300 rounded-md">
-                    <input class="" />
-                    <p class="bg-gray-100">测试代码</p>
-                </div>
-                <div class="text-center border border-gray-300 rounded-md">
-                    <p class="bg-gray-100">第二个div</p>
-                </div>
-                <div class="text-center border border-gray-300 rounded-md">
-                    <p class="bg-gray-100">
-                        第二个divwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-                    </p>
-                </div>
-                <div class="text-center grid-flow-row border">
-                    <input
-                        type="text"
-                        id="last_name"
-                        class="bg-gray-50 border border-gray-300 rounded-md"
-                        placeholder="Doe"
-                        required
-                    />
-                </div>
-            </div>
-        </div>
-        <div class="grid grid-flow-row auto-rows-max">
-            <div class="text-center border border-x-gray-200 rounded-md">
-                <p class="bg-gray-100">进行中</p>
-            </div>
-        </div>
-        <div class="grid grid-flow-row auto-rows-max">
-            <div class="text-center border border-x-gray-200 rounded-md">
-                <p class="bg-gray-100">已完成</p>
-            </div>
-        </div>
-        <div class="grid grid-flow-row auto-rows-max">
-            <div class="text-center border border-x-gray-200 rounded-md">
-                <p class="bg-gray-100">归档</p>
-            </div>
-        </div>
-    </div>
+<!-- -- div在宽度居中 -->
+<div class="container mx-auto w-6/12 border-b boder-gray-300 py-4">
+    <ul class="flex space-x-8">
+        <li>
+            <a
+                class="hover:text-sky-500 dark:hover:text-sky-400 hover:font-bold"
+                href="">待办事项</a
+            >
+        </li>
+        <li>
+            <a href="" class="hover:text-sky-500 dark:hover:text-sky-400"
+                >正在进行</a
+            >
+        </li>
+        <li>
+            <a
+                class="hover:text-sky-500 dark:hover:text-sky-400 font-normal"
+                href="">已经完成</a
+            >
+        </li>
+        <li>
+            <a class="hover:text-sky-500 dark:hover:text-sky-400" href=""
+                >归档</a
+            >
+        </li>
+    </ul>
+</div>
+
+<div class="container mx-auto w-6/12 border-b boder-gray-300 py-4">
+    <ul
+        class="max-auto space-y-2 text-gray-700 list-disc list-inside dark:text-gray-400"
+    >
+        <li class="mb-2">
+            学会使用Svelte afaaafaf aaaaaaaaaaaaaaaaaaaaaaaaaaaa afafafaaaaa
+            afafaffa afafafafa afafafafa af afaf afafaf
+            <button
+                type="button"
+                class="text-white px-2 bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg"
+                >Doing</button
+            >
+        </li>
+        <li class="mb-2">
+            学会使用Svelte afaaafaf aaaaaaaaaaaaaaaaaaaaaaaaaaaa afafafaaaaa
+            afafaffa afafafafa afafafafa af afaf afaa
+            <button
+                type="button"
+                class="text-white px-2 bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg"
+                >Doing</button
+            >
+        </li>
+    </ul>
 </div>
