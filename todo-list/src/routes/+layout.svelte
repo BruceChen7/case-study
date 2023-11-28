d8b92554aae33dd486963e9c2bf60672f4ca6ba0
BruceChen7 <bofengqiye7@gmail.com>
2023-11-28 17:41:50 +0800

    feat: add todolist

---
+++ todo-list/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="grid grid-cols-3 gap-7 h-screen">
+    <div
+        class="border-color-red-500 text-center border border-red-500 h-1/3 mt-1/3"
+    >
+        第一个div
+    </div>
+    <div class="text-center border">第二个div</div>
+    <div class="text-center border">第三个div</div>
+</div>
---
+++ todo-list/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<!--定义的是一个页面的header结构-->
+<!-- 每个page都会有 -->
+<svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+    <h1 class="text-15xl font-bold underline">Hello world!</h1>
+    <h1>
+        <span class="welcome">
+            <picture> </picture>
+        </span>
+
+        to your new<br />SvelteKit app
+    </h1>
+
+    <h2 class="text-center">
+        try editing <strong>src/routes/+page.svelte</strong>
+    </h2>
+
+    <p class="bg-red-500 text-3xl">Hello from page</p>
+</section>
+
+<style lang="postcss">
+</style>
