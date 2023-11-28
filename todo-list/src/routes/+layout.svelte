4d1798c4efd5b756c18e87d4f40de3a5870b1057
BruceChen7 <bofengqiye7@gmail.com>
2023-11-28 19:05:17 +0800

    feat: add todolist

--- todo-list/src/routes/+layout.svelte
+++ todo-list/src/routes/+layout.svelte
@@ -2,12 +2,50 @@
     import "../app.css";
 </script>
 
-<div class="grid grid-cols-3 gap-7 h-screen">
-    <div
-        class="border-color-red-500 text-center border border-red-500 h-1/3 mt-1/3"
-    >
-        第一个div
+<div class="mt-52">
+    <div class="grid grid-cols-4 gap-7">
+        <div class="grid grid-flow-row auto-rows-max">
+            <div class="text-center border border-x-gray-200 rounded-md">
+                <p class="bg-green-500">待办列表</p>
+            </div>
+            <div class="border-gray-300">
+                <div class="text-center border border-gray-300 rounded-md">
+                    <input class="" />
+                    <p class="bg-gray-100">测试代码</p>
+                </div>
+                <div class="text-center border border-gray-300 rounded-md">
+                    <p class="bg-gray-100">第二个div</p>
+                </div>
+                <div class="text-center border border-gray-300 rounded-md">
+                    <p class="bg-gray-100">
+                        第二个divwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+                    </p>
+                </div>
+                <div class="text-center grid-flow-row border">
+                    <input
+                        type="text"
+                        id="last_name"
+                        class="bg-gray-50 border border-gray-300 rounded-md"
+                        placeholder="Doe"
+                        required
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-flow-row auto-rows-max">
+            <div class="text-center border border-x-gray-200 rounded-md">
+                <p class="bg-gray-100">进行中</p>
+            </div>
+        </div>
+        <div class="grid grid-flow-row auto-rows-max">
+            <div class="text-center border border-x-gray-200 rounded-md">
+                <p class="bg-gray-100">已完成</p>
+            </div>
+        </div>
+        <div class="grid grid-flow-row auto-rows-max">
+            <div class="text-center border border-x-gray-200 rounded-md">
+                <p class="bg-gray-100">归档</p>
+            </div>
+        </div>
     </div>
-    <div class="text-center border">第二个div</div>
-    <div class="text-center border">第三个div</div>
 </div>
