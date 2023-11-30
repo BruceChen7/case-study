d5f8fcb1ca9334d216f7a7401c95e773831c8866
BruceChen7 <bofengqiye7@gmail.com>
2023-11-30 22:42:56 +0800

    feat: make it dynamic

--- todo-list/src/routes/+layout.svelte
+++ todo-list/src/routes/+layout.svelte
@@ -30,42 +30,6 @@
     </ul>
 </div>
 
-<div class="container mx-auto w-6/12 py-4">
-    <ul
-        class="max-auto space-y-2 text-gray-700 list-disc list-inside dark:text-gray-400"
-    >
-        <li class="flex mb-2 list-disc marker:text-blue-200 justify-between">
-            <p class="list-disc space-x-2 w-3/4">
-                学会使用Svelte afaaafaf aaaaaaaaaaaaaaaaaaaaaaaaaaaa afafafaaaaa
-                学会使用Svelte afaaafaf aaaaaaaaaaaaaaaaaaaaaaaaaaaa afafafaaaaa
-                学会使用Svelte afaaafaf aaaaaaaaaaaaaaaaaaaaaaaaaaaa afafafaaaaa
-            </p>
-            <button
-                type="button"
-                class="h-3/4 text-white justify-end ml-2 px-2 bg-gray-700 hover:bg-blue-800 space-x-2 font-medium rounded-lg"
-                >Add</button
-            >
-            <button
-                type="button"
-                class="h-3/4 text-white ml-2 justify-end px-2 space-x-2 bg-gray-700 hover:bg-blue-800 font-medium rounded-lg"
-                >Doing</button
-            >
-        </li>
-        <li class="flex mb-4 mr-0 justify-between">
-            <p class="w-3/4">
-                学会使用Svelte afaaafaf aaaaaaaaaaaaaaaaaaaaaaaaaaaa afafafaaaaa
-                afafaffa afaf
-            </p>
-            <button
-                type="button"
-                class="h-3/4 text-white ml-2 px-2 bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg"
-                >Add</button
-            >
-            <button
-                type="button"
-                class="h-3/4 text-white ml-2 px-2 bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg"
-                >Doing</button
-            >
-        </li>
-    </ul>
-</div>
+<section>
+    <slot />
+</section>
--- todo-list/src/routes/+page.svelte
+++ todo-list/src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
+    // array of string
+
+    let data: string[] = [];
+    onMount(async () => {
+        const resp = await fetch("http://127.0.0.1:8080/");
+        const res = await resp.json();
+        console.log(res);
+        data = res.data;
+    });
 </script>
 
 <!--定义的是一个页面的header结构-->
@@ -8,22 +18,31 @@
     <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-    <h1 class="text-15xl font-bold underline">Hello world!</h1>
-    <h1>
-        <span class="welcome">
-            <picture> </picture>
-        </span>
-
-        to your new<br />SvelteKit app
-    </h1>
-
-    <h2 class="text-center">
-        try editing <strong>src/routes/+page.svelte</strong>
-    </h2>
-
-    <p class="bg-red-500 text-3xl">Hello from page</p>
-</section>
+<div class="container mx-auto w-6/12 py-4">
+    <ul
+        class="max-auto space-y-2 text-gray-700 list-disc list-inside dark:text-gray-400"
+    >
+        {#each data as row}
+            <li
+                class="flex mb-2 list-disc marker:text-blue-200 justify-between"
+            >
+                <p class="list-disc space-x-2 w-3/4">
+                    {row}
+                </p>
+                <button
+                    type="button"
+                    class="h-3/4 text-white justify-end ml-2 px-2 bg-gray-700 hover:bg-blue-800 space-x-2 font-medium rounded-lg"
+                    >Add</button
+                >
+                <button
+                    type="button"
+                    class="h-3/4 text-white ml-2 justify-end px-2 space-x-2 bg-gray-700 hover:bg-blue-800 font-medium rounded-lg"
+                    >Doing</button
+                >
+            </li>
+        {/each}
+    </ul>
+</div>
 
 <style lang="postcss">
 </style>
